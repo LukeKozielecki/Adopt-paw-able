383ef8006acc5d8a8ed0dea97b9ff561e2dfeee9
Luke <lucjan.kozielecki@gmail.com>
2024-09-18 13:25:16 +0200

    Initial empty app commit

---
+++ app/src/main/java/luke/koz/adopt_paw_able/MainActivity.kt
@@ -0,0 +1,33 @@
+package luke.koz.adopt_paw_able
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import luke.koz.adopt_paw_able.ui.theme.AdoptpawableTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AdoptpawableTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        Text(text = "Placeholder_text")
+                    }
+                }
+            }
+        }
+    }
+}
